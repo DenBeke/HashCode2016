aa7de3059da9bdeeb9020a292fcdf9b356115f78
Jakob <jakob@WirelessStudenten-143169205197.uantwerpen.be>
2016-02-11 19:58:50 +0100

    Naive implementation of routing, fairly untested :)

--- main.py
+++ main.py
@@ -5,6 +5,23 @@ class Drone:
 		self.x = x
 		self.y = y
 		self.counter = 0
+		self.currentPayload = 0
+
+	def addPayload(self, load):
+		self.currentPayload += load
+
+	def payloadLeft(self):
+		return maxLoad - self.currentPayload
+
+	def moveTo(self, x, y):
+		self.x = x
+		self.y = y
+
+	def load(self, item):
+		pass
+
+	def deliver(self):
+		pass
 
 	def __str__(self):
 		return str(self.x) + " " + str(self.y)
@@ -26,12 +43,53 @@ class Order:
 		self.x = x
 		self.y = y
 		self.items = items
+		self.currentItem = 0
 
 	def __str__(self):
 		out = "  " + str(self.x) + " " + str(self.y) + " " +  "items:"
 		for item in self.items:
 			out += " " + str(item)
 		return out
+
+
+	def getNextItem(self):
+		if self.currentItem == len(self.items):
+			return None
+		return self.items[self.currentItem]
+
+	def removeNextItem(self):
+		self.currentItem += 1
+
+	def hasItems(self):
+		return self.currentItem < len(self.items)
+
+def findWarehouse(typeName):
+	for warehouse in warehouses:
+		if warehouse.items[typeName] > 0:
+			warehouse.items[typeName] -= 1
+			return [warehouse.x, warehouse.y]
+
+def findImminentDrone():
+	return min(drones, key = lambda drone: drone.counter)
+
+
+def processOrders(orders):
+	for order in orders:
+	    drone = findImminentDrone()
+	    while order.hasItems() :
+			item = order.getNextItem()
+			while drone.payloadLeft() >= productTypes[item]:
+				order.removeNextItem()
+				warehouse = findWarehouse(item)
+				drone.moveTo(warehouse[0], warehouse[1])
+				drone.load(item)
+				item = order.getNextItem()
+				if item == None:
+					break
+			drone.moveTo(order.x, order.y)
+			drone.deliver()
+
+
 
 
 height, width, numDrones, numTurns, maxLoad = map(int, sys.stdin.readline().split())
@@ -84,6 +142,5 @@ print("--------------------------\n")
 # Init drones
 drones = [Drone(0,0) for _ in range(numDrones)]
 
+processOrders(orders)
 
-def findImminentDrone():
-	return min(drones, key = lambda drone: drone.counter)
